72232856f59a0ef3f9e3ae16bd8ca550dcc0d414
vimal-oneway <vimalic555@gmail.com>
2024-01-24 23:36:26 +0530

    backup

--- src/components/templates/Homepage/FlowGraph.tsx
+++ src/components/templates/Homepage/FlowGraph.tsx
@@ -5,11 +5,11 @@ export const FlowGraph = () => {
   return (
     <div className="mt-52 ">
       <div className="w-container pt-28">
-        <h3 className=" mx-auto w-3/5 text-center font-gotham text-[34px] font-medium leading-10 tracking-[-0.68px]">
+        <h3 className=" mx-auto w-3/5 text-center font-gotham text-[24px] font-medium leading-[33px] tracking-[-0.48px] md:text-[34px] md:leading-[48px] md:tracking-[-0.68px]">
           Observability That’s Built for{" "}
           <span className="text-accent-light">Developers Like YOU</span>
         </h3>
-        <p className="mx-auto w-3/5 text-center font-medium">
+        <p className="mx-auto my-6 w-3/6 text-center font-medium">
           Incerto’s unique approach leverages the trifecta of OpenTelemetry for
           unified telemetry collection, Clichouse to speed up queries from any
           data source, and Grafana to visualise Metrics, Logs, and Traces in a
