5ab1ed49973a46d973dcb522efef73712c4d28cd
GOKUL S <vimalic555@gmail.com>
2024-02-05 17:01:17 +0530

    Hero page fix

--- src/components/modules/Heading.tsx
+++ src/components/modules/Heading.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/elements/Button";
 import { ArrowOutward } from "@/components/elements/icons/ArrowOutward";
+import Image from "next/image";
 import Link from "next/link";
 
 export const Heading = () => {
   return (
-    <section className="w-container mx-auto mt-24 flex flex-col items-center justify-center gap-[32px] px-6 md:mt-36 md:w-5/6 md:gap-[34px]">
+    <section className="w-container mt-24 flex flex-col items-center justify-center gap-[32px] px-6 md:mt-[54px] md:w-5/6 md:gap-[34px]">
       <div className="mx-auto w-full xl:w-5/6 3xl:w-4/6">
         <h1 className="text-center font-gotham text-[30px] font-medium leading-[42px] tracking-[-1.2px] md:text-[44px] md:font-medium md:leading-[60px] md:tracking-tightest">
           <span>
@@ -19,13 +20,23 @@ export const Heading = () => {
       </p>
       <Link
         href={"#contact"}
-        className="rounded-[14px] bg-accent px-[18px] py-2.5 font-[800] text-secondary"
+        className="rounded-[14px] bg-accent px-[18px] py-2.5 font-[600] text-secondary"
       >
         <div className="flex items-center justify-center gap-2.5">
           <p>Get in touch</p>
           <ArrowOutward />
         </div>
       </Link>
+
+      <div className="mt-[38px] flex w-full justify-center">
+        <Image
+          src={"/backgrounds/dashboard.png"}
+          alt="Dashboard"
+          width={1080}
+          height={1080}
+          className="object-contain"
+        />
+      </div>
     </section>
   );
 };
--- src/components/templates/Homepage/WhyUs.tsx
+++ src/components/templates/Homepage/WhyUs.tsx
@@ -30,7 +30,7 @@ export const WhyUs = () => {
         </div>
 
         {/* Cards */}
-        <div className="flex w-full flex-col items-center justify-center gap-96">
+        <div className="flex w-full flex-col items-center justify-center gap-[28px]">
           {data.data.map((item, index) => {
             return (
               <WhyUsCard
