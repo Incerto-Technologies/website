157f2e201332ce1191b53992e726de636b4d9f2f
vimal-oneway <vimalic555@gmail.com>
2024-02-08 22:38:49 +0530

    blog card update

--- src/components/modules/BlogCard.tsx
+++ src/components/modules/BlogCard.tsx
@@ -6,34 +6,38 @@ import { Blog } from "@/types/Blogs";
 export const BlogCard = ({ image, title, description, author, _id }: Blog) => {
   return (
     <div className="mx-auto  h-full min-h-[372px] w-4/5 max-w-[350px] md:w-full">
-      <div className="relative aspect-[4/3] w-full overflow-hidden rounded-[30px]">
-        <Image
-          src={image}
-          alt={title}
-          height={370}
-          width={300}
-          className="object-contian h-full w-full"
-        />
-        <div className="absolute bottom-5 left-1/2 flex w-full max-w-[170px] -translate-x-1/2 items-center justify-center gap-2.5 rounded-[20px] bg-white px-4 py-2.5">
+      <Link href={getBlogUrl(_id)}>
+        <div className="relative aspect-[4/3] w-full overflow-hidden rounded-[30px]">
           <Image
-            src={"/logos/avatar/kunalsingh.webp"}
-            alt={author}
-            width={30}
-            height={30}
-            className="h-[30px] w-[30px] rounded-full object-cover"
+            src={image}
+            alt={title}
+            height={370}
+            width={300}
+            className="object-contian h-full w-full"
           />
-          <p className="w-full font-semibold text-primary">{author}</p>
+          <div className="absolute bottom-5 left-1/2 flex w-full max-w-[170px] -translate-x-1/2 items-center justify-center gap-2.5 rounded-[20px] bg-white px-4 py-2.5">
+            <Image
+              src={"/logos/avatar/kunalsingh.webp"}
+              alt={author}
+              width={30}
+              height={30}
+              className="h-[30px] w-[30px] rounded-full object-cover"
+            />
+            <p className="w-full font-semibold text-primary">{author}</p>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className="bg- red-400 mt-11 flex flex-col gap-6 md:h-[304px] md:justify-between md:gap-0">
-        <h3 className="font-blod text-xl leading-8 tracking-wide">
-          {title.length > 60 ? `${title.slice(0, 60)}...` : title}
-        </h3>
-        <p className="font-medium text-[#9E9E9E]">
-          {description.length > 123
-            ? `${description.slice(0, 123)}...`
-            : description}
-        </p>
+        <Link href={getBlogUrl(_id)}>
+          <h3 className="font-blod text-xl leading-8 tracking-wide">
+            {title.length > 60 ? `${title.slice(0, 60)}...` : title}
+          </h3>
+          <p className="font-medium text-[#9E9E9E]">
+            {description.length > 123
+              ? `${description.slice(0, 123)}...`
+              : description}
+          </p>
+        </Link>
         <Link
           href={getBlogUrl(_id)}
           className="w-full rounded-[20px] border border-[#0F937C] bg-transparent px-5 py-[14px]"
--- src/components/templates/About/Team.tsx
+++ src/components/templates/About/Team.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 import { v4 as uuid } from "uuid";
 export const Team = () => {
   return (
-    <section className="mt-[150px] px-5 md:mt-[200px]">
+    <section className="w-container mt-[150px] px-5 md:mt-[200px]">
       <h3 className="text-center text-[24px] font-medium  tracking-tighter text-[#EDEDED] md:text-[34px] md:leading-[58px] md:tracking-[-0.68px]">
         People powering incerto
       </h3>
-      <div className="mt-10 flex flex-col gap-[50px] md:flex-row md:flex-wrap">
+      <div className="mt-10 flex flex-col gap-x-[110px] gap-y-28 md:flex-row md:flex-wrap">
         {teamMembers.map((member) => (
-          <div key={uuid()} className="md:flex ">
+          <div key={uuid()} className="w-full max-w-[508px] md:flex">
             <Image
               src={member.image}
               alt={member.name}
               width={1080}
               height={1080}
-              className="rounded-[20px]"
+              className="rounded-[20px] md:aspect-square md:w-[250px]"
             />
             <div>
               <h4 className="mt-5 text-[18px] font-semibold tracking-tight text-[#EDEDED]">
