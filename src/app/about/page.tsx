581b9f32f862da0882032faf6a425b5558b4be4d
vimal-oneway <vimalic555@gmail.com>
2024-02-08 18:05:52 +0530

    deploy blog

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,14 @@
+import { DefaultLayout } from "@/components/layouts/DefaultLayout";
+
 export default function page() {
-  return <div>page</div>;
+  return (
+    <DefaultLayout>
+      <div className="flex min-h-screen w-full items-center justify-center">
+        <h1 className="text-4xl font-semibold text-secondary">
+          404 | No blog found
+        </h1>
+      </div>
+    </DefaultLayout>
+  );
 }
+// hi
