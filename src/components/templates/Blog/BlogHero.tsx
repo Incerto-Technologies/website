db3b7899d91dcdc4bb12287c1952fe185797ddcc
vimal-oneway <vimalic555@gmail.com>
2024-02-13 14:58:15 +0530

    add mobile filter on blog page

--- src/components/templates/Blog/BlogHero.tsx
+++ src/components/templates/Blog/BlogHero.tsx
@@ -5,10 +5,11 @@ import { useAppDispatch, useTypedSelector } from "@/hooks/store";
 import { setSearch, setSearchTag } from "@/store/slice/blog.slice";
 import { Tags } from "@/types/Blogs";
 import { classNameMerge } from "@/utils/classNameMerge";
-
+import { useState } from "react";
 export const BlogHero = () => {
   const dispatch = useAppDispatch();
-  const { Blogs, search, searchTag } = useTypedSelector((state) => state.blog);
+  const [open, setOpen] = useState(false);
+  const { searchTag } = useTypedSelector((state) => state.blog);
   return (
     <section className="home-background flex h-full flex-col justify-center">
       <div className="w-container align-center mt-20 w-5/6 md:pt-44">
@@ -16,7 +17,7 @@ export const BlogHero = () => {
           Discover cool updates and blogs here
         </h1>
       </div>
-      <div className="w-container mt-[72px] flex justify-between gap-[18px] px-[20px] md:mt-[137px]  md:justify-start md:gap-[30px] md:px-0">
+      <div className="w-container relative mt-[72px] flex justify-between gap-[18px] px-[20px] md:mt-[137px]  md:justify-start md:gap-[30px] md:px-0">
         <div className="flex items-center gap-2 rounded-2xl bg-[#232323] px-5 py-2.5 md:w-3/5">
           <SearchIcon />
           <input
@@ -27,8 +28,13 @@ export const BlogHero = () => {
             }}
           />
         </div>
-        <div className="md:hidden">
-          <Button className="flex h-[44px] w-[44px] justify-center rounded-[16px] border-none bg-[#232323]">
+        <div className="w-[2/5] md:hidden">
+          <Button
+            onClick={() => {
+              setOpen(!open);
+            }}
+            className="flex h-[44px] w-[44px] justify-center rounded-[16px] border-none bg-[#232323]"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="shrink-0"
@@ -76,7 +82,36 @@ export const BlogHero = () => {
               {tag}
             </Button>
           ))}
-        </div>
+        </div>{" "}
+        {open && (
+          <div
+            className="absolute right-0 w-full px-5"
+            style={{
+              top: "calc(100% + 20px)",
+            }}
+          >
+            <div className="w-full rounded-[16px] bg-[#232323]  p-6">
+              <div className="flex flex-col">
+                {Tags.map((tag, index) => (
+                  <div key={`${tag}--${index}`}>
+                    <Button
+                      className={classNameMerge(
+                        "rounded-2xl bg-[#232323] font-[500] capitalize",
+                      )}
+                      onClick={() => {
+                        // @ts-ignore
+                        dispatch(setSearchTag(tag));
+                      }}
+                    >
+                      {tag}
+                    </Button>
+                    <div className="my-1 h-[1px] w-full bg-[#3A3A3A]"></div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
