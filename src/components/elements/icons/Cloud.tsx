bbc24bf2f674c2dd9e9d69c5cea1af235b891ccc
vimal-oneway <vimalic555@gmail.com>
2024-02-15 17:39:19 +0530

    product page opt

---
+++ src/components/elements/icons/Cloud.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export const Cloud = ({ size }: { size: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 34 34"
+      fill="none"
+    >
+      <path
+        d="M7.84848 15.7529C1.21848 16.2204 1.21848 25.8679 7.84848 26.3354H10.5685"
+        stroke="#00FF99"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.91937 15.7531C3.37187 3.10226 22.5535 -1.95524 24.7493 11.3331C30.8835 12.1123 33.3627 20.2864 28.716 24.3522C27.2993 25.6414 25.4718 26.3497 23.5593 26.3356H23.4318"
+        stroke="#00FF99"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M24.0834 23.4173C24.0834 24.4657 23.8568 25.4573 23.4318 26.3357C23.3184 26.5907 23.1909 26.8315 23.0492 27.0582C21.8309 29.1123 19.5784 30.5007 17.0001 30.5007C14.4217 30.5007 12.1692 29.1123 10.9509 27.0582C10.8092 26.8315 10.6818 26.5907 10.5684 26.3357C10.1434 25.4573 9.91675 24.4657 9.91675 23.4173C9.91675 19.5073 13.0901 16.334 17.0001 16.334C20.9101 16.334 24.0834 19.5073 24.0834 23.4173Z"
+        stroke="#00FF99"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.7898 23.4176L16.1923 24.8203L19.2098 22.0293"
+        stroke="#00FF99"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
