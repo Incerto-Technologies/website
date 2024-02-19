f1ac8f1d53a0fa50be603a529218bfbc5722eb56
vimal-oneway <vimalic555@gmail.com>
2024-02-19 22:18:36 +0530

    Animation fix

--- src/components/modules/Heading.tsx
+++ src/components/modules/Heading.tsx
@@ -10,13 +10,13 @@ export const Heading = () => {
       {/* <section> */}
       <section className="w-container mt-[54px] flex flex-col items-center justify-center gap-[32px] px-6 md:w-5/6 md:gap-[34px]">
         <div className="mx-auto w-full lg:w-5/6 2xl:w-4/6">
-          <h1 className="relative text-center font-gotham text-[30px] font-[900]  leading-[40px] tracking-[-1.2px] md:text-[44px] md:leading-[50px] md:tracking-tightest">
+          <h1 className="relative text-center font-gotham text-[30px] font-bold leading-[40px]  tracking-[-1.2px] md:text-[44px] md:font-medium md:leading-[50px] md:tracking-tightest">
             <TextAnimation text={heading} once className="" el="span" />
             <motion.span
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 1.5, ease: "easeInOut" }}
-              className="animate-gradient-text relative bottom-[8px]"
+              className="animate-gradient-text relative bottom-[8px] font-[900]"
             >
               Your Infrastructure
             </motion.span>
