e5b7ebfe563ca8017add3c1d3f14d2cfaba60d6c
vimal-oneway <vimalic555@gmail.com>
2024-02-24 16:18:34 +0530

    video add, 404 page and hover effect for all cta

--- src/components/modules/BookDemo.tsx
+++ src/components/modules/BookDemo.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/elements/Button";
 export const BookDemo = () => {
   const router = useRouter();
   return (
-    <div className="mt-10 flex w-full items-center justify-center">
+    <div className="mt-10 flex w-full items-center justify-center ">
       <Button
         className="transition-all duration-200 ease-in hover:bg-[#035749]"
         onClick={() => {
