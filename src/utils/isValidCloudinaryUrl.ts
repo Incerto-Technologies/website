c120b5b6ad01e09d0c7250a2519fde430348c175
vimal-oneway <vimalic555@gmail.com>
2024-04-09 18:41:45 +0530

    testing

--- src/utils/isValidCloudinaryUrl.ts
+++ src/utils/isValidCloudinaryUrl.ts
@@ -1,6 +1,6 @@
 export const isValidCloudinaryUrl = (url: string): boolean => {
   const cloudinaryUrlPattern =
-    /^https?:\/\/res\.cloudinary\.com\/[a-z0-9_-]+\/image\/upload\/v[0-9]+\/[a-z0-9_./-]+$/;
+    /^https?:\/\/res\.cloudinary\.com\/[a-z0-9_-]+\/image\/upload/;
 
   return cloudinaryUrlPattern.test(url);
 };
