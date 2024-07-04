93f568213f70520cd886a07ef020ea1343dc74d0
vimal-oneway <vimalic555@gmail.com>
2024-07-04 18:48:56 +0530

    design review

--- src/components/modules/BlogCard.tsx
+++ src/components/modules/BlogCard.tsx
@@ -4,9 +4,9 @@ import { BlogLink } from "./BlogLink";
 
 export const BlogCard = ({ image, title, description, author, _id }: Blog) => {
   return (
-    <div className="mx-auto  h-full min-h-[372px] w-4/5 max-w-[350px] md:w-full">
+    <div className="mx-auto p-2  h-full min-h-[372px] w-4/5 max-w-[350px] md:w-full rounded-xl border border-gray-700 bg-gray-900 overflow-hidden">
       <BlogLink title={title}>
-        <div className="relative aspect-[4/3] w-full overflow-hidden rounded-[30px]">
+        <div className="relative aspect-[4/3] w-full overflow-hidden rounded-md">
           <Image
             src={image}
             alt={title}
@@ -15,20 +15,15 @@ export const BlogCard = ({ image, title, description, author, _id }: Blog) => {
             className="object-contian h-full w-full"
           />
         </div>
-        <div className="bg- red-400 mt-11 flex flex-col gap-6 md:h-[110px] md:justify-between md:gap-0">
+        <div className="mt-6 flex flex-col gap-6 md: md:justify-between md:gap-0">
           <h3
             className="font-blod text-xl leading-8 tracking-wide"
             style={{
               fontWeight: 600,
             }}
           >
-            {title.length > 60 ? `${title.slice(0, 60)}...` : title}
+            {title}
           </h3>
-          <p className="font-medium leading-6 text-[#9E9E9E] mt-2">
-            {description.length > 123
-              ? `${description.slice(0, 123)}...`
-              : description}
-          </p>
         </div>
       </BlogLink>
     </div>
--- src/components/templates/Home/Customers.tsx
+++ src/components/templates/Home/Customers.tsx
@@ -20,42 +20,44 @@ const CustomersLogoData = [
     width: 120
   },
   {
-    src: "/logos/customers/nuvoretail.svg",
-    alt: "nuva-retail logo",
-    height: 80,
-    width: 120
-  },
-  {
     src: "/logos/customers/ving.svg",
     alt: "ving logo",
     height: 80,
     width: 120
   },
   {
-    src: "/logos/customers/Kevit.svg",
+    src: "/logos/customers/kevit.svg",
     alt: "kevit logo",
-    height: 50,
-    width: 100
+    height: 80,
+    width: 120
   },
   {
     src: "/logos/customers/astra.svg",
     alt: "astra logo",
-    height: 50,
-    width: 100
+    height: 80,
+    width: 120
   },
 ];
 
 export const Customers = () => {
   return (
-    <div className="w-container grid grid-cols-2 gap-12 md:gap-10 w-3/4 md:grid-cols-4">
-      {CustomersLogoData.map(({ src, alt, width, height }, i) => (
-        <div className={`h-[${height}px] mt-10 md:mt-0 flex items-center justify-center`} key={i}>
-          <div className={`max-w-[${width}px]`}>
-            <Image src={src} height={height} className={`object-contain h-[${height}] w-[${width}]`} width={width} alt={alt} />
-          </div>
-        </div >
-      ))}
+    <div>
+      <h2 className="text-center font-gotham text-2xl font-medium leading-[33px] tracking-[-0.48px] md:text-[34px] md:leading-[48px] md:tracking-[-0.68px]">
+        Trusted by{" "}
+        <span className="features-title text-clip-all">
+          teams
+        </span>
+        {" "}
+        at
+      </h2>
+      <div className="grid grid-cols-2 max-w-[1000px] gap-12 md:gap-10 w-3/4 md:w-1/2 mx-auto md:grid-cols-3 mt-20">
+        {CustomersLogoData.map(({ src, alt, width, height }, i) => (
+          <div className={`h-[120px] mt-10 md:mt-0 flex justify-start`} key={i}>
+            <Image src={src} className="h-[50px] w-full object-contain" height={height} width={width} alt={alt} />
+          </div >
+        ))}
 
-    </div >
+      </div >
+    </div>
   );
 };
