ce42fc0cbb9c08c1e3898fc2c126e91d894bd7d1
Vimal raj R <vimal@Vimals-MacBook-Pro.local>
2024-07-29 18:33:12 +0530

    changed resolution of blog card image

--- src/components/modules/BlogCard.tsx
+++ src/components/modules/BlogCard.tsx
@@ -4,18 +4,18 @@ import { BlogLink } from "./BlogLink";
 
 export const BlogCard = ({ image, title, description, author, _id }: Blog) => {
   return (
-    <div className="mx-auto p-2  h-full min-h-[372px] w-4/5 max-w-[350px] md:w-full rounded-xl border border-gray-700 bg-gray-900 overflow-hidden">
+    <div className="mx-auto h-full  min-h-[372px] w-4/5 max-w-[350px] overflow-hidden rounded-xl border border-gray-700 bg-gray-900 p-2 md:w-full">
       <BlogLink title={title}>
         <div className="relative aspect-[4/3] w-full overflow-hidden rounded-md">
           <Image
             src={image}
             alt={title}
-            height={370}
-            width={300}
+            height={770}
+            width={900}
             className="object-contian h-full w-full"
           />
         </div>
-        <div className="mt-6 flex flex-col gap-6 md: md:justify-between md:gap-0">
+        <div className="md: mt-6 flex flex-col gap-6 md:justify-between md:gap-0">
           <h3
             className="font-blod text-xl leading-8 tracking-wide"
             style={{
--- src/components/modules/BlogCardMobile.tsx
+++ src/components/modules/BlogCardMobile.tsx
@@ -9,18 +9,23 @@ interface Props extends Blog {
 export const BlogCardMobile = ({ image, title, author, _id }: Props) => {
   return (
     <BlogLink title={title}>
-      <div className="mt-16 mx-[20px] rounded-xl border border-gray-700 bg-gray-900 p-[12px]">
+      <div className="mx-[20px] mt-16 rounded-xl border border-gray-700 bg-gray-900 p-[12px]">
         <Image
-          className="rounded-md w-full h-full object-cover"
+          className="h-full w-full rounded-md object-cover"
           src={image}
           alt={title}
-          width={350}
-          height={200}
-
+          height={770}
+          width={900}
         />
         <h3 className="mt-[24px] font-bold leading-6">{title}</h3>
         <div className="mt-4 flex items-center gap-2.5">
-          <Image className="rounded-xl" src={author.profile || ""} alt="author_profile" width={25} height={25} />
+          <Image
+            className="rounded-xl"
+            src={author.profile || ""}
+            alt="author_profile"
+            width={25}
+            height={25}
+          />
           <p className="text-sm capitalize">{author.userName}</p>
         </div>
       </div>
